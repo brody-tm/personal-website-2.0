259711434f40f525d8d29ec89a8a2b04622120ad
brodytrue <btm00005@mix.wvu.edu>
2025-03-18 14:00:26 -0400

    updated animation

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-gsap.from("header", {
-    y: "-100%",
-    duration: 1.5,
-    ease: "bounce"
-})
+gsap.fromTo(".social-links", 
+    {y: "30%", opacity: 0}, 
+    {y: "0%", opacity: 1}
+)
+
+gsap.fromTo("header", 
+    {opacity: 0}, 
+    {opacity: 1, delay: .25}
+)
