3d2ebb3927dfb01ae140c23200904be1d7918f92
brodytrue <btm00005@mix.wvu.edu>
2025-03-18 17:44:05 -0400

    fixed flashing

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-gsap.fromTo(".social-links", 
-    {y: "30%", opacity: 0}, 
-    {y: "0%", opacity: 1}
-)
+var tl = gsap.timeline();
 
-gsap.fromTo("header", 
-    {opacity: 0}, 
-    {opacity: 1, delay: .25}
-)
+tl.to("body", { opacity: 1 });
+tl.from("header", { y: -50, opacity: 0 }, 0);
+tl.from(".social-links", { y: 50, opacity: 0 }, ">");
+tl.from(".projects", { opacity: 0 }, ">");
+tl.from("footer", { y: 50, opacity: 0 }, 0);
+
+tl.duration(1.5);
